ee6e65c8a8020dfc137c31b9b915209f3bca565b
Daniel Knaack <daniel.knaack@protonmail.com>
2023-05-09 19:20:17 +0200

    read and print the source code

--- src/main.c
+++ src/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct string {
+	char *at;
+	size_t length;
+};
+
+struct string
+read_file(char *filename)
+{
+	struct string result = {0};
+	FILE *file = fopen(filename, "rb");
+	if (file) {
+		fseek(file, 0, SEEK_END);
+		result.length = ftell(file);
+		fseek(file, 0, SEEK_SET);
+
+		result.at = malloc(result.length + 1);
+		if (result.at) {
+			fread(result.at, result.length, 1, file);
+			result.at[result.length] = '\0';
+		}
+
+		fclose(file);
+	}
+
+	return result;
+}
 
 int
 main(void)
 {
-	printf("Hello, world!\n");
+	struct string contents = read_file("src/main.c");
+	fwrite(contents.at, 1, contents.length, stdout);
+	free(contents.at);
 	return 0;
 }
