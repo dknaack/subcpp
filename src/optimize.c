11afa1deff1cb096fafa024177d8ebe9dc7f4653
Daniel Knaack <daniel.knaack@protonmail.com>
2023-10-13 12:16:22 +0200

    misc: formatting

--- src/optimize.c
+++ src/optimize.c
@@ -31,20 +31,22 @@ optimize(struct ir_program program, struct arena *arena)
 		switch (instrs[i].opcode) {
 		case IR_ADD:
 			if (instrs[op0].opcode == IR_CONST
-			    && instrs[op1].opcode == IR_CONST) {
+				&& instrs[op1].opcode == IR_CONST)
+			{
 				instrs[i].opcode = IR_ADD;
 				instrs[op1].opcode = IR_NOP;
 				instrs[op0].opcode = IR_NOP;
 				instrs[i].op0 = add(
-				    instrs[op0].op0,
-				    instrs[op1].op0);
+					instrs[op0].op0,
+					instrs[op1].op0);
 			} else if (instrs[op0].opcode == IR_CONST
-			    && instrs[op0].op0 == 0) {
+				&& instrs[op0].op0 == 0)
+			{
 				instrs[op0].opcode = IR_NOP;
 				instrs[i].opcode = IR_MOV;
 				instrs[i].op0 = i;
 			} else if (instrs[op1].opcode == IR_CONST
-			    && instrs[op1].op0 == 0) {
+				&& instrs[op1].op0 == 0) {
 				instrs[op1].opcode = IR_NOP;
 				instrs[i].opcode = IR_MOV;
 				instrs[i].op1 = op0;
@@ -53,15 +55,17 @@ optimize(struct ir_program program, struct arena *arena)
 			break;
 		case IR_SUB:
 			if (instrs[op0].opcode == IR_CONST
-			    && instrs[op1].opcode == IR_CONST) {
+				&& instrs[op1].opcode == IR_CONST)
+			{
 				instrs[i].opcode = IR_CONST;
 				instrs[op1].opcode = IR_NOP;
 				instrs[op0].opcode = IR_NOP;
 				instrs[i].op0 = sub(
-				    instrs[op0].op0,
-				    instrs[op1].op0);
+					instrs[op0].op0,
+					instrs[op1].op0);
 			} else if (instrs[op1].opcode == IR_CONST
-			    && instrs[op1].op0 == 0) {
+				&& instrs[op1].op0 == 0)
+			{
 				instrs[op1].opcode = IR_NOP;
 				instrs[i].opcode = IR_MOV;
 				instrs[i].op1 = op0;
@@ -70,20 +74,22 @@ optimize(struct ir_program program, struct arena *arena)
 			break;
 		case IR_MUL:
 			if (instrs[op0].opcode == IR_CONST
-			    && instrs[op1].opcode == IR_CONST) {
+				&& instrs[op1].opcode == IR_CONST) {
 				instrs[i].opcode = IR_CONST;
 				instrs[op1].opcode = IR_NOP;
 				instrs[op0].opcode = IR_NOP;
 				/* TODO: evaluate depending on the target architecture */
 				instrs[i].op0 = multiply(
-				    instrs[op0].op0,
-				    instrs[op1].op0);
+					instrs[op0].op0,
+					instrs[op1].op0);
 			} else if (instrs[op0].opcode == IR_CONST
-			    && instrs[op0].op0 == 1) {
+				&& instrs[op0].op0 == 1)
+			{
 				instrs[i].opcode = IR_MOV;
 				instrs[i].op0 = i;
 			} else if (instrs[op1].opcode == IR_CONST
-			    && instrs[op1].op0 == 1) {
+				&& instrs[op1].op0 == 1)
+			{
 				instrs[i].opcode = IR_MOV;
 				instrs[i].op1 = op0;
 				instrs[i].op0 = i;
@@ -91,7 +97,8 @@ optimize(struct ir_program program, struct arena *arena)
 			break;
 		case IR_JIZ:
 			if (instrs[op0].opcode == IR_CONST
-			    && instrs[op0].op0 == 0) {
+				&& instrs[op0].op0 == 0)
+			{
 				instrs[op0].opcode = IR_NOP;
 				instrs[i].opcode = IR_JMP;
 				instrs[i].op0 = instrs[i].op1;
@@ -109,7 +116,7 @@ optimize(struct ir_program program, struct arena *arena)
 		(void)op1;
 
 		while (instrs[op1].opcode == IR_MOV
-		    && instrs[op1].op0 == op1) {
+			&& instrs[op1].op0 == op1) {
 			instrs[op1].opcode = IR_NOP;
 			instrs[i].op1 = instrs[op1].op1;
 		}
