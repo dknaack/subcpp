6f8049f19732f4dac628beb51fffd1bc333969b5
Daniel Knaack <daniel.knaack@protonmail.com>
2023-12-24 11:59:19 +0100

    fix stack variable promotion

--- src/optimize.c
+++ src/optimize.c
@@ -20,22 +20,23 @@ multiply(u32 a, u32 b)
 }
 
 static void
-optimize(ir_program program, arena *arena)
+promote_stack_variables(ir_program program, arena *arena)
 {
 	ir_instr *instrs = program.instrs;
+	arena_temp temp = arena_temp_begin(arena);
 
+	// Mark all stack variables which address is used by a different
+	// instruction than load/store.
 	b32 *addr_used = ALLOC(arena, program.instr_count, b32);
 	for (u32 i = 0; i < program.instr_count; i++) {
 		u32 opcode = instrs[i].opcode;
 		u32 op0 = instrs[i].op0;
 		u32 op1 = instrs[i].op1;
 
-		if (opcode == IR_LOAD || opcode == IR_STORE) {
-			continue;
-		}
-
 		ir_opcode_info info = get_opcode_info(opcode);
-		if (info.op0 == IR_OPERAND_REG_SRC && instrs[op0].opcode == IR_ALLOC) {
+		if (opcode != IR_LOAD && info.op0 == IR_OPERAND_REG_SRC
+			&& instrs[op0].opcode == IR_ALLOC)
+		{
 			addr_used[op0] |= true;
 		}
 
@@ -44,13 +45,12 @@ optimize(ir_program program, arena *arena)
 		}
 	}
 
+	// Promote stack variables to registers
 	for (u32 i = 0; i < program.instr_count; i++) {
 		if (instrs[i].opcode == IR_LOAD) {
 			u32 op0 = instrs[i].op0;
 			if (instrs[op0].opcode == IR_ALLOC && !addr_used[op0]) {
-				instrs[i].opcode = IR_MOV;
-				instrs[i].op0 = i;
-				instrs[i].op1 = op0;
+				instrs[i].opcode = IR_COPY;
 			}
 		} else if (instrs[i].opcode == IR_STORE) {
 			u32 op0 = instrs[i].op0;
@@ -63,10 +63,20 @@ optimize(ir_program program, arena *arena)
 	for (u32 i = 0; i < program.instr_count; i++) {
 		if (instrs[i].opcode == IR_ALLOC) {
 			if (!addr_used[i]) {
-				instrs[i].opcode = IR_NOP;
+				instrs[i].opcode = IR_VAR;
 			}
 		}
 	}
+
+	arena_temp_end(temp);
+}
+
+static void
+optimize(ir_program program, arena *arena)
+{
+	ir_instr *instrs = program.instrs;
+
+	promote_stack_variables(program, arena);
 
 	for (u32 i = 0; i < program.instr_count; i++) {
 		u32 op0 = instrs[i].op0;
