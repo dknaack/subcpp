dd6fecaf7b7392b200ef126fa95c15c9fead07f0
Daniel Knaack <daniel.knaack@protonmail.com>
2024-03-05 15:44:59 +0100

    remove old tests

--- test/for.cpp
+++
@@ -1,9 +0,0 @@
-int
-main(void)
-{
-	for (int i = 0; i < 100; i = i + 1) {
-		print i;
-	}
-
-	return 0;
-}
--- test/types.cpp
+++
@@ -1,9 +0,0 @@
-int
-main(void)
-{
-	int c = 0;
-	int *p = &c;
-	*p = 5;
-	c = *p;
-	print c;
-}
