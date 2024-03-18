80422a1f7ef432d4d9ca3f0eeae3ddb380a01d23
Daniel Knaack <daniel.knaack@protonmail.com>
2024-03-18 16:48:35 +0100

    build: add more warnings/errors

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 mkdir -p build
-cc -g3 -Wall -Wextra -Wno-missing-field-initializers -Wno-unused-function -o build/subcpp src/main.c
+cc -g3 -Wall -Wextra -Wno-missing-field-initializers -Wno-unused-function \
+	-Werror=implicit-function-declaration -Werror=return-type \
+	-o build/subcpp src/main.c
