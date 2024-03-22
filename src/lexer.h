201121447ff9cb4b5340615e7201d6856e924dff
Daniel Knaack <daniel.knaack@protonmail.com>
2024-03-22 19:51:08 +0100

    minor changes
    Order the token declarations alphabetically.

--- src/lexer.h
+++ src/lexer.h
@@ -19,32 +19,32 @@ typedef enum {
 	TOKEN_COLON,
 	TOKEN_COMMA,
 	TOKEN_DOT,
+	TOKEN_ELLIPSIS,
 	TOKEN_EQUAL,
 	TOKEN_EQUAL_EQUAL,
-	TOKEN_ELLIPSIS,
-	TOKEN_RSHIFT,
-	TOKEN_GREATER_EQUAL,
 	TOKEN_GREATER,
+	TOKEN_GREATER_EQUAL,
 	TOKEN_HASH,
 	TOKEN_HASH_HASH,
 	TOKEN_IDENT,
 	TOKEN_LBRACE,
 	TOKEN_LBRACKET,
-	TOKEN_LSHIFT,
+	TOKEN_LESS,
 	TOKEN_LESS_EQUAL,
 	TOKEN_LPAREN,
-	TOKEN_LESS,
+	TOKEN_LSHIFT,
 	TOKEN_MINUS,
-	TOKEN_MINUS_MINUS,
 	TOKEN_MINUS_EQUAL,
+	TOKEN_MINUS_MINUS,
 	TOKEN_PERCENT,
 	TOKEN_PERCENT_EQUAL,
 	TOKEN_PLUS,
-	TOKEN_PLUS_PLUS,
 	TOKEN_PLUS_EQUAL,
+	TOKEN_PLUS_PLUS,
 	TOKEN_RBRACE,
 	TOKEN_RBRACKET,
 	TOKEN_RPAREN,
+	TOKEN_RSHIFT,
 	TOKEN_SEMICOLON,
 	TOKEN_SLASH,
 	TOKEN_SLASH_EQUAL,
@@ -53,8 +53,8 @@ typedef enum {
 	TOKEN_TILDE,
 
 	// literals
-	TOKEN_LITERAL_INT,
 	TOKEN_LITERAL_FLOAT,
+	TOKEN_LITERAL_INT,
 	TOKEN_LITERAL_STRING,
 
 	// keywords
@@ -131,56 +131,56 @@ get_token_name(token_kind kind)
 	switch (kind) {
 	case TOKEN_INVALID:       return "invalid token";
 	case TOKEN_EOF:           return "eof";
-	case TOKEN_COMMA:         return "','";
-	case TOKEN_COLON:         return "':'";
+	case TOKEN_NEWLINE:       return "newline";
+	case TOKEN_WHITESPACE:    return "whitespace";
 	case TOKEN_AMP:           return "'&'";
 	case TOKEN_AMP_AMP:       return "'&&'";
 	case TOKEN_AMP_EQUAL:     return "'&='";
 	case TOKEN_ARROW:         return "'->'";
-	case TOKEN_BANG:          return "'!'";
 	case TOKEN_BACKSLASH:     return "'\\'";
+	case TOKEN_BANG:          return "'!'";
 	case TOKEN_BAR:           return "'|'";
 	case TOKEN_BAR_BAR:       return "'||'";
 	case TOKEN_BAR_EQUAL:     return "'|='";
 	case TOKEN_CARET:         return "'^'";
 	case TOKEN_CARET_EQUAL:   return "'^='";
+	case TOKEN_COLON:         return "':'";
+	case TOKEN_COMMA:         return "','";
 	case TOKEN_DOT:           return "'.'";
-	case TOKEN_PLUS:          return "'+'";
-	case TOKEN_PLUS_PLUS:     return "'++'";
-	case TOKEN_PLUS_EQUAL:    return "'+='";
-	case TOKEN_MINUS:         return "'-'";
-	case TOKEN_MINUS_MINUS:   return "'--'";
-	case TOKEN_MINUS_EQUAL:   return "'-='";
-	case TOKEN_STAR:          return "'*'";
-	case TOKEN_STAR_EQUAL:    return "'*='";
-	case TOKEN_SLASH:         return "'/'";
-	case TOKEN_SLASH_EQUAL:   return "'/='";
-	case TOKEN_PERCENT:       return "'%'";
-	case TOKEN_PERCENT_EQUAL: return "'%='";
-	case TOKEN_EQUAL_EQUAL:   return "'=='";
 	case TOKEN_ELLIPSIS:      return "'...'";
-	case TOKEN_LESS:          return "'<'";
-	case TOKEN_LSHIFT:        return "'<<'";
+	case TOKEN_EQUAL:         return "'='";
+	case TOKEN_EQUAL_EQUAL:   return "'=='";
 	case TOKEN_GREATER:       return "'>'";
-	case TOKEN_LESS_EQUAL:    return "'<='";
 	case TOKEN_GREATER_EQUAL: return "'>='";
-	case TOKEN_RSHIFT:        return "'>>'";
 	case TOKEN_HASH:          return "'#'";
 	case TOKEN_HASH_HASH:     return "'##'";
-	case TOKEN_EQUAL:         return "'='";
 	case TOKEN_IDENT:         return "identifier";
-	case TOKEN_NEWLINE:       return "newline";
-	case TOKEN_WHITESPACE:    return "whitespace";
-	case TOKEN_SEMICOLON:     return "';'";
-	case TOKEN_LPAREN:        return "'('";
-	case TOKEN_RPAREN:        return "')'";
-	case TOKEN_LBRACKET:      return "'['";
-	case TOKEN_RBRACKET:      return "']'";
 	case TOKEN_LBRACE:        return "'{'";
+	case TOKEN_LBRACKET:      return "'['";
+	case TOKEN_LESS:          return "'<'";
+	case TOKEN_LESS_EQUAL:    return "'<='";
+	case TOKEN_LPAREN:        return "'('";
+	case TOKEN_LSHIFT:        return "'<<'";
+	case TOKEN_MINUS:         return "'-'";
+	case TOKEN_MINUS_EQUAL:   return "'-='";
+	case TOKEN_MINUS_MINUS:   return "'--'";
+	case TOKEN_PERCENT:       return "'%'";
+	case TOKEN_PERCENT_EQUAL: return "'%='";
+	case TOKEN_PLUS:          return "'+'";
+	case TOKEN_PLUS_EQUAL:    return "'+='";
+	case TOKEN_PLUS_PLUS:     return "'++'";
 	case TOKEN_RBRACE:        return "'}'";
+	case TOKEN_RBRACKET:      return "']'";
+	case TOKEN_RPAREN:        return "')'";
+	case TOKEN_RSHIFT:        return "'>>'";
+	case TOKEN_SEMICOLON:     return "';'";
+	case TOKEN_SLASH:         return "'/'";
+	case TOKEN_SLASH_EQUAL:   return "'/='";
+	case TOKEN_STAR:          return "'*'";
+	case TOKEN_STAR_EQUAL:    return "'*='";
 	case TOKEN_TILDE:         return "'~'";
-	case TOKEN_LITERAL_INT:   return "integer";
 	case TOKEN_LITERAL_FLOAT: return "float";
+	case TOKEN_LITERAL_INT:   return "integer";
 	case TOKEN_LITERAL_STRING: return "string";
 	case TOKEN_AUTO:          return "'auto'";
 	case TOKEN_BREAK:         return "'break'";
@@ -188,11 +188,11 @@ get_token_name(token_kind kind)
 	case TOKEN_CHAR:          return "'char'";
 	case TOKEN_CONST:         return "'const'";
 	case TOKEN_CONTINUE:      return "'continue'";
-	case TOKEN_DO:            return "'do'";
 	case TOKEN_DEFAULT:       return "'default'";
-	case TOKEN_EXTERN:        return "'extern'";
+	case TOKEN_DO:            return "'do'";
 	case TOKEN_ELSE:          return "'else'";
 	case TOKEN_ENUM:          return "'enum'";
+	case TOKEN_EXTERN:        return "'extern'";
 	case TOKEN_FLOAT:         return "'float'";
 	case TOKEN_FOR:           return "'for'";
 	case TOKEN_GOTO:          return "'goto'";
@@ -202,8 +202,8 @@ get_token_name(token_kind kind)
 	case TOKEN_REGISTER:      return "'register'";
 	case TOKEN_RESTRICT:      return "'restrict'";
 	case TOKEN_RETURN:        return "'return'";
-	case TOKEN_SIGNED:        return "'signed'";
 	case TOKEN_SHORT:         return "'short'";
+	case TOKEN_SIGNED:        return "'signed'";
 	case TOKEN_STATIC:        return "'static'";
 	case TOKEN_STRUCT:        return "'struct'";
 	case TOKEN_SWITCH:        return "'switch'";
