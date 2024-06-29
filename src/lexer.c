639238a43133d283820bf7c8c41671c08fa2a33f
Daniel Knaack <daniel.knaack@protonmail.com>
2024-06-29 11:21:02 +0200

    define __gnuc_va_list in stdarg.h
    Because GNU people like to define their own types and they seem to not
    provide any typedef using regular va_list, we have to define this type
    ourselves...

--- src/lexer.c
+++ src/lexer.c
@@ -1087,6 +1087,7 @@ get_token(cpp_state *cpp)
 							"#ifndef __STDARG_H\n"
 							"#define __STDARG_H\n"
 							"typedef __builtin_va_list va_list;\n"
+							"typedef __builtin_va_list __gnuc_va_list;\n"
 							"#define va_start(ap, ptr) __builtin_va_start(ap, ptr)\n"
 							"#define va_end(ap)        __builtin_va_end(ap)\n"
 							"#define va_arg(ap, type)  __builtin_va_arg(ap, type)\n"
