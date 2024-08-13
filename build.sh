501bf40ef967e542a82ac69dde986bdb6f87f718
Daniel Knaack <daniel.knaack@protonmail.com>
2024-08-13 16:27:34 +0200

    enable debug build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 cc -g3 -Wall -Wextra -Wno-missing-field-initializers -Wno-unused-function \
-	-Werror=implicit-function-declaration -Werror=return-type \
+	-Werror=implicit-function-declaration -Werror=return-type -DDEBUG=1 \
 	-o build/subcpp src/main.c
