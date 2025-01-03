eb81f2c8bb83712dcab3d5cce20e4bdb3bf0754e
Daniel Knaack <daniel.knaack@protonmail.com>
2025-01-03 10:20:06 +0100

    Move simple functions to header

--- src/regalloc.c
+++ src/regalloc.c
@@ -1,34 +1,3 @@
-static bitset
-new_bitset(i32 size, arena *perm)
-{
-	bitset result = {0};
-	result.size = size;
-	result.bits = ALLOC(perm, (size + 31) / 32, b32);
-	return result;
-}
-
-static void
-set_bit(bitset set, i32 i, b32 value)
-{
-	set.bits[i / 32] &= ~(1 << (i % 32));
-	set.bits[i / 32] |= (value != 0) << (i % 32);
-}
-
-static b32
-get_bit(bitset set, i32 i)
-{
-	b32 result = (set.bits[i / 32] >> (i % 32)) & 1;
-	return result;
-}
-
-static void
-swap_u32(u32 *a, isize i, isize j)
-{
-	u32 tmp = a[i];
-	a[i] = a[j];
-	a[j] = tmp;
-}
-
 static u32 *
 regalloc(mach_token *tokens, isize token_count,
 	basic_block *blocks, isize block_count, mach_info mach, arena *arena)
--- src/regalloc.h
+++ src/regalloc.h
@@ -7,3 +7,34 @@ typedef struct {
 	b32 *bits;
 	i32 size;
 } bitset;
+
+static bitset
+new_bitset(i32 size, arena *perm)
+{
+	bitset result = {0};
+	result.size = size;
+	result.bits = ALLOC(perm, (size + 31) / 32, b32);
+	return result;
+}
+
+static void
+set_bit(bitset set, i32 i, b32 value)
+{
+	set.bits[i / 32] &= ~(1 << (i % 32));
+	set.bits[i / 32] |= (value != 0) << (i % 32);
+}
+
+static b32
+get_bit(bitset set, i32 i)
+{
+	b32 result = (set.bits[i / 32] >> (i % 32)) & 1;
+	return result;
+}
+
+static void
+swap_u32(u32 *a, isize i, isize j)
+{
+	u32 tmp = a[i];
+	a[i] = a[j];
+	a[j] = tmp;
+}
