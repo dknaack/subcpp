5f88636ce6a200f1016b1bc79672a665b43d85be
Daniel Knaack <daniel.knaack@protonmail.com>
2025-01-11 17:12:00 +0100

    Sanitize undefined behavior

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 mkdir -p build
 cc -g3 -Wall -Wextra -Wno-missing-field-initializers -Wno-unused-function \
 	-Werror=implicit-function-declaration -Werror=return-type -DDEBUG=1 \
+	-fsanitize-trap -fsanitize=undefined \
 	-o build/subcpp src/main.c
