ea103c0ce2213bb93d4fa9f1dabb7e6a46403af4
Daniel Knaack <daniel.knaack@protonmail.com>
2025-09-19 17:54:33 +0200

    Do optimization per function

--- src/optimize.c
+++ src/optimize.c
@@ -5,14 +5,6 @@ typedef struct {
 	isize register_count;
 } pointer_info;
 
-typedef struct {
-	i32 start;
-	i32 size;
-	i32 succ[2];
-	i32 *pred;
-	i32 pred_count;
-} ir_block;
-
 static u32
 add(u32 a, u32 b)
 {
@@ -83,71 +75,73 @@ join_pointer_sets(pointer_info *info, isize dst, isize src)
 static void
 optimize(ir_program program, arena *arena)
 {
-	// Build a CFG
-	isize block_count = 0;
-	for (isize i = 0; i < program.inst_count; i++) {
-		ir_opcode opcode = program.insts[i].opcode;
-		if (opcode == IR_JMP || opcode == IR_JIZ || opcode == IR_JNZ) {
-			block_count++;
-		}
-	}
+	for (isize func_id = 0; func_id < program.func_count; func_id++) {
+		ir_function *func = &program.funcs[func_id];
+		ir_inst *insts = func->insts;
+		arena_temp temp = arena_temp_begin(arena);
 
-	ir_block *blocks = ALLOC(arena, block_count, ir_block);
-	isize block_index = 0;
-	for (isize i = 0; i < program.inst_count; i++) {
-		ir_opcode opcode = program.insts[i].opcode;
-		if (opcode == IR_JMP || opcode == IR_JIZ || opcode == IR_JNZ) {
-			blocks[block_index].size = i + 1 - blocks[block_index].start;
-			block_index++;
-			blocks[block_index].start = i + 1;
+		//
+		// CFG Construction
+		//
+
+		isize block_count = 0;
+		for (isize i = 0; i < func->inst_count; i++) {
+			ir_opcode opcode = insts[i].opcode;
+			if (opcode == IR_JMP || opcode == IR_JIZ || opcode == IR_JNZ) {
+				block_count++;
+			}
 		}
 
-		if (opcode == IR_JMP) {
-			i32 arg0 = program.insts[i].args[0];
-			blocks[arg0].pred_count++;
-			blocks[block_index - 1].succ[0] = arg0;
-			blocks[block_index - 1].succ[1] = arg0;
-		} else if (opcode == IR_JIZ || opcode == IR_JNZ) {
-			i32 arg1 = program.insts[i].args[1];
-			blocks[arg1].pred_count++;
-			blocks[block_index].pred_count++;
-			blocks[block_index - 1].succ[0] = block_index;
-			blocks[block_index - 1].succ[1] = arg1;
+		ir_block *blocks = ALLOC(arena, block_count, ir_block);
+		isize block_index = 0;
+		for (isize i = 0; i < func->inst_count; i++) {
+			ir_opcode opcode = insts[i].opcode;
+			if (opcode == IR_JMP || opcode == IR_JIZ || opcode == IR_JNZ) {
+				blocks[block_index++].end = i + 1;
+				blocks[block_index].begin = i + 1;
+			}
+
+			if (opcode == IR_JMP) {
+				i32 arg0 = insts[i].args[0];
+				blocks[arg0].pred_count++;
+				blocks[block_index - 1].succ[0] = arg0;
+				blocks[block_index - 1].succ[1] = arg0;
+			} else if (opcode == IR_JIZ || opcode == IR_JNZ) {
+				i32 arg1 = insts[i].args[1];
+				blocks[arg1].pred_count++;
+				blocks[block_index].pred_count++;
+				blocks[block_index - 1].succ[0] = block_index;
+				blocks[block_index - 1].succ[1] = arg1;
+			}
 		}
-	}
 
-	i32 pred_offset = 0;
-	i32 *block_preds = ALLOC(arena, block_count, i32);
-	for (isize i = 0; i < block_count; i++) {
-		pred_offset += blocks[i].pred_count;
-		blocks[i].pred = block_preds + pred_offset;
-	}
-
-	block_index = 0;
-	for (isize i = 0; i < program.inst_count; i++) {
-		ir_opcode opcode = program.insts[i].opcode;
-		if (opcode == IR_JMP) {
-			i32 arg0 = program.insts[i].args[0];
-			*--blocks[arg0].pred = block_index;
-			block_index++;
-		} else if (opcode == IR_JIZ || opcode == IR_JNZ) {
-			i32 arg1 = program.insts[i].args[1];
-			*--blocks[arg1].pred = block_index;
-			*--blocks[block_index + 1].pred = block_index;
-			block_index++;
+		i32 pred_offset = 0;
+		i32 *block_preds = ALLOC(arena, block_count, i32);
+		for (isize i = 0; i < block_count; i++) {
+			pred_offset += blocks[i].pred_count;
+			blocks[i].pred = block_preds + pred_offset;
 		}
-	}
 
+		block_index = 0;
+		for (isize i = 0; i < func->inst_count; i++) {
+			ir_opcode opcode = insts[i].opcode;
+			if (opcode == IR_JMP) {
+				i32 arg0 = insts[i].args[0];
+				*--blocks[arg0].pred = block_index;
+				block_index++;
+			} else if (opcode == IR_JIZ || opcode == IR_JNZ) {
+				i32 arg1 = insts[i].args[1];
+				*--blocks[arg1].pred = block_index;
+				*--blocks[block_index + 1].pred = block_index;
+				block_index++;
+			}
+		}
 
-	// Promote stack variables
-	for (isize f = 0; f < program.func_count; f++) {
-		ir_function *func = &program.funcs[f];
-		// Mark all stack variables which address is used by a different
-		// instruction than load/store.
-		ir_inst *insts = program.insts + func->inst_index;
+		//
+		// Promote stack variables
+		//
 
 		// Determine what each variable can point to
-		arena_temp temp = arena_temp_begin(arena);
 		pointer_info pointer_info = {0};
 		pointer_info.register_count = func->inst_count;
 		pointer_info.rank = ALLOC(arena, pointer_info.register_count, i8);
@@ -217,13 +211,10 @@ optimize(ir_program program, arena *arena)
 			}
 		}
 
-		arena_temp_end(temp);
-	}
+		//
+		// Constant folding
+		//
 
-	// Constant-folding
-	for (isize i = 0; i < program.func_count; i++) {
-		ir_function *func = &program.funcs[i];
-		ir_inst *insts = program.insts + func->inst_index;
 		for (isize i = 0; i < func->inst_count; i++) {
 			u32 arg0 = insts[i].args[0];
 			u32 arg1 = insts[i].args[1];
@@ -303,9 +294,54 @@ optimize(ir_program program, arena *arena)
 				break;
 			}
 		}
+
+		// Remove register copies from the IR tree
+		for (isize i = 0; i < func->inst_count; i++) {
+			ir_opcode_info info = get_opcode_info(insts[i].opcode);
+
+			u32 arg0 = insts[i].args[0];
+			if (info.usage[0] != 0 && insts[arg0].opcode == IR_COPY) {
+				insts[i].args[0] = insts[arg0].args[0];
+			}
+
+			u32 arg1 = insts[i].args[1];
+			if (info.usage[1] != 0 && insts[arg1].opcode == IR_COPY) {
+				insts[i].args[1] = insts[arg1].args[0];
+			}
+		}
+
+		// Remove unused registers
+		i32 *ref_count = get_ref_count(insts, func->inst_count, arena);
+		for (isize j = 0; j < func->inst_count; j++) {
+			b32 has_side_effect;
+			ir_opcode opcode = insts[j].opcode;
+			switch (opcode) {
+			case IR_ALLOC:
+			case IR_LABEL:
+			case IR_PARAM:
+			case IR_LOAD:
+			case IR_STORE:
+			case IR_CALL:
+			case IR_JIZ:
+			case IR_JMP:
+			case IR_JNZ:
+			case IR_RET:
+				has_side_effect = true;
+				break;
+			default:
+				has_side_effect = false;
+				break;
+			}
+			if (!(ref_count[j] >= 1 || has_side_effect)) {
+				insts[j].opcode = IR_NOP;
+			}
+		}
+
+		arena_temp_end(temp);
 	}
 
 	// Eliminate dead code
+#if 0
 	{
 		b8 *reachable = ALLOC(arena, program.max_label_count, b8);
 		u32 *stack = ALLOC(arena, program.max_label_count, u32);
@@ -323,7 +359,7 @@ optimize(ir_program program, arena *arena)
 				}
 			}
 
-			// Do a depth first search on the labels, start with the first
+			// Do a depth first search on the labels, begin with the first
 			// instruction in the function.
 			i32 stack_pos = 0;
 			stack[stack_pos++] = 1;
@@ -333,8 +369,8 @@ next_block:
 				isize label = stack[--stack_pos];
 
 				// NOTE: The first instruction is the label itself
-				u32 start = label_addresses[label];
-				for (isize j = start; j < func->inst_count; j++) {
+				u32 begin = label_addresses[label];
+				for (isize j = begin; j < func->inst_count; j++) {
 					u32 i = inst[j].args[0];
 					u32 new_label = 0;
 					switch (inst[i].opcode) {
@@ -368,10 +404,10 @@ next_block:
 					continue;
 				}
 
-				u32 start = label_addresses[label];
+				u32 begin = label_addresses[label];
 				// Remove the label
-				inst[inst[start].args[0]].opcode = IR_NOP;
-				for (isize j = start; j; j = inst[j].args[1]) {
+				inst[inst[begin].args[0]].opcode = IR_NOP;
+				for (isize j = begin; j; j = inst[j].args[1]) {
 					u32 i = inst[j].args[0];
 					if (inst[i].opcode == IR_LABEL) {
 						break;
@@ -382,58 +418,5 @@ next_block:
 			}
 		}
 	}
-
-	// Remove register copies from the IR tree
-	for (isize i = 0; i < program.func_count; i++) {
-		ir_function *func = &program.funcs[i];
-		ir_inst *insts = program.insts + func->inst_index;
-		for (isize i = 0; i < func->inst_count; i++) {
-			ir_opcode_info info = get_opcode_info(insts[i].opcode);
-
-			u32 arg0 = insts[i].args[0];
-			if (info.usage[0] != 0 && insts[arg0].opcode == IR_COPY) {
-				insts[i].args[0] = insts[arg0].args[0];
-			}
-
-			u32 arg1 = insts[i].args[1];
-			if (info.usage[1] != 0 && insts[arg1].opcode == IR_COPY) {
-				insts[i].args[1] = insts[arg1].args[0];
-			}
-		}
-	}
-
-	// Remove unused registers
-	for (isize i = 0; i < program.func_count; i++) {
-		ir_function *func = &program.funcs[i];
-		arena_temp temp = arena_temp_begin(arena);
-		ir_inst *inst = program.insts + func->inst_index;
-
-		i32 *ref_count = get_ref_count(inst, func->inst_count, arena);
-		for (isize j = 0; j < func->inst_count; j++) {
-			b32 has_side_effect;
-			ir_opcode opcode = inst[j].opcode;
-			switch (opcode) {
-			case IR_ALLOC:
-			case IR_LABEL:
-			case IR_PARAM:
-			case IR_LOAD:
-			case IR_STORE:
-			case IR_CALL:
-			case IR_JIZ:
-			case IR_JMP:
-			case IR_JNZ:
-			case IR_RET:
-				has_side_effect = true;
-				break;
-			default:
-				has_side_effect = false;
-				break;
-			}
-			if (!(ref_count[j] >= 1 || has_side_effect)) {
-				inst[j].opcode = IR_NOP;
-			}
-		}
-
-		arena_temp_end(temp);
-	}
+#endif
 }
