359e4d026c8d6934633aaccbd6d9bf7a4efe7dca
Daniel Knaack <daniel.knaack@protonmail.com>
2025-11-12 11:14:16 +0100

    Ignore unused parameters

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p build
-cc -g3 -Wall -Wextra -Wno-missing-field-initializers -Wno-unused-function \
+cc -g3 -Wall -Wextra -Wno-missing-field-initializers -Wno-unused-function -Wno-unused-parameter \
 	-Werror=implicit-function-declaration -Werror=return-type -DDEBUG=1 \
 	-fsanitize-trap -fsanitize=undefined \
 	-o build/subcpp src/main.c
